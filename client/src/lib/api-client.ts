3a7812369b6e250cbcb954791b3499d7a8be7b3c
rakesh b <rakeshbsirvi46@gmail.com>
2025-06-18 01:15:30 +0530

    chore : moved for folder restructure

---
+++ client/src/lib/api-client.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosHeaders } from 'axios';
+
+console.log(import.meta.env.VITE_API_URL)
+// Create an Axios instance with default config
+export const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  withCredentials: true,
+});
+
+apiClient.interceptors.request.use(
+  async (config) => {
+    if (config.data instanceof FormData && config.headers) {
+      (config.headers as AxiosHeaders).set('Content-Type', 'multipart/form-data');
+    }
+
+    return config;
+  },
+  (error) => {
+    console.error('Request interceptor error:', error);
+    return Promise.reject(error);
+  }
+);
+
+export const api = {
+  joinWaitlist: (email: string) => apiClient.post('/join-waitlist', { email }),
+};
