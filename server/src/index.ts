4ed240b2cf7dc11d79b7c2c5243277afb224c262
rakesh b <rakeshbsirvi46@gmail.com>
2025-06-18 15:11:06 +0530

    feat : track user route implemented for tracking unique users

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express";
 import rateLimit from "express-rate-limit";
 import { supabase } from "./supabase-client";
+import { sendDiscordWebhook } from "./utils/webhook";
 import cors from "cors";
-import dotenv from "dotenv"
-
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
@@ -11,11 +11,12 @@ const port = 3000;
 
 app.use(express.json());
 
-app.use(cors({
-  origin: process.env.ALLOWED_ORIGIN,
-  credentials: true
-}));
-
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGIN,
+    credentials: true,
+  })
+);
 
 const joinWaitlistLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
@@ -30,26 +31,60 @@ app.post(
     try {
       const { email } = req.body;
       if (!email) {
-        
         res.status(400).json({ message: "Email is required" });
         return;
       }
       const { data, error } = await supabase
-      .from("email_waitlist")
-      .insert({ email: req.body.email });
+        .from("email_waitlist")
+        .insert({ email: req.body.email });
+
+      if (error) {
+        let message = error.message;
+        if (error.code === "23505") {
+          message =
+            "Already signed up, looks like you can't wait for the launch";
+        }
+        res.status(400).json({ message: message });
+        return;
+      }
+
+      res.status(200).json("You are Subscribed");
+    } catch (error) {
+      res.status(500).json("Internal server error");
+    }
+  }
+);
+
+app.post(
+  "/track-user",
+  joinWaitlistLimiter,
+  async (req: Request, res: Response) => {
+    try {
+      const { fingerprint } = req.body;
+
+      if(!fingerprint || fingerprint.fingerprint_hash){
+        res.status(200).json("Fingerprint missing");
+      }
+
+      const { data, error } = await supabase.from("user_fingerprints").select("*").eq("fingerprint_hash", fingerprint.fingerprint_hash).single();
       
       if(error){
-            let message = error.message;
-            if(error.code === '23505'){
-              message = "Already signed up, looks like you can't wait for the launch"
+        if(data === null){
+          const { data, error } = await supabase.from("user_fingerprints").insert(fingerprint).select().single();
+
+          if(!error){
+            const webhookUrl = process.env.WAITLIST_WEBHOOK_URL;
+
+            if(webhookUrl){
+              await sendDiscordWebhook(webhookUrl, "Visitor", `IP : ${data.ip_address}`, "#3498DB")
             }
-            res.status(400).json( { message: message } );
-            return;
+          }
         }
-        
-        res.status(200).json("You are Subscribed");
+      }
+      
+      res.status(200);
     } catch (error) {
-        res.status(500).json("Internal server error");   
+      res.status(500).json("Internal server error");
     }
   }
 );
